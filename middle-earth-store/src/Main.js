c66f8897116bdb4bb0858a96bedf3023f466bee9
Azurebird <white.camilo@gmail.com>
2018-09-16 22:14:55 -0500

    Subtotal increasing correctly

--- middle-earth-store/src/Main.js
+++ middle-earth-store/src/Main.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import {Route, NavLink, HashRouter} from "react-router-dom"
-import Products from "./com/mh/product/Products"
+import Index from "./com/mh/index/Index"
+import Products from "./com/mh/product/ShoppingStore"
 import Cart from "./com/mh/cart/Cart"
 import Contact from "./com/mh/contact/Contact"
 import "./index.css"
@@ -11,13 +12,17 @@ class Main extends Component {
         <HashRouter>
             <div>
             <h1>Welcome to Middle Earth Store</h1>
-            <ul className="header">
-                <li><NavLink exact to="/">Products</NavLink></li>
-                <li><NavLink to="/cart">Cart</NavLink></li>
-                <li><NavLink to="/contact">Contact</NavLink></li>
-            </ul>
+            <div className="navBar">
+                <NavLink className="navBar-brand" exact to="/">Middle Earth</NavLink>
+                <ul className="header">
+                    <li><NavLink to="/products">Products</NavLink></li>
+                    <li><NavLink to="/cart">Cart</NavLink></li>
+                    <li><NavLink to="/contact">Contact</NavLink></li>
+                </ul>
+            </div>
             <div className="content">
-                <Route exact path="/" component={Products}/>
+                <Route exact path="/" component={Index}/>
+                <Route path="/products" component={Products}/>
                 <Route path="/cart" component={Cart}/>
                 <Route path="/contact" component={Contact}/>
             </div>
---
+++ middle-earth-store/src/com/mh/index/Index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Index extends Component {
+    render() {
+        return (
+            <div>
+                Hola a todos, bienvenidos a nuestra tienda
+            </div>
+          );
+    }
+}
+
+export default Index;
